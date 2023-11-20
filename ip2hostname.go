1c2f020e25f18333d13aae054975401eb672119f
Bamorph <142395807+Bamorph@users.noreply.github.com>
2023-11-20 14:54:36 +1100

    Update ip2hostname.go
    upgrade with added waitgroups

--- ip2hostname.go
+++ ip2hostname.go
@@ -1,38 +1,67 @@
 package main
 
 import (
-        "bufio"
-        "fmt"
-        "net"
-        "os"
-        "strings"
+	"bufio"
+	"flag"
+	"fmt"
+	"net"
+	"os"
+	"strings"
+	"sync"
 )
 
-func getHostnames(ip string) ([]string, error) {
-        hostnames, _ := net.LookupAddr(ip)
-        return hostnames, nil
+func main() {
+	// Parse command line flags
+	concurrency := flag.Int("c", 1, "Number of concurrent workers")
+	flag.Parse()
+
+	// Create a wait group to wait for all workers to finish
+	var wg sync.WaitGroup
+
+	// Create a channel to send IP addresses to workers
+	ipChannel := make(chan string)
+
+	// Start workers
+	for i := 0; i < *concurrency; i++ {
+		wg.Add(1)
+		go worker(&wg, ipChannel)
+	}
+
+	// Read IP addresses from standard input and send them to the channel
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		ipAddress := scanner.Text()
+		ipChannel <- ipAddress
+	}
+
+	// Close the channel once all IP addresses are sent
+	close(ipChannel)
+
+	// Wait for all workers to finish
+	wg.Wait()
 }
 
-func main() {
-        var ipAddress string
+func worker(wg *sync.WaitGroup, ipChannel <-chan string) {
+	defer wg.Done()
 
-        if len(os.Args) > 1 {
-                ipAddress = os.Args[1]
-        } else {
+	for ipAddress := range ipChannel {
+		hostname := resolveHostname(ipAddress)
+		if hostname != "" {
+			fmt.Println(hostname)
+		}
+	}
+}
 
-        scanner := bufio.NewScanner(os.Stdin)
+func resolveHostname(ipAddress string) string {
+	// Use net.LookupAddr to resolve the hostname
+	hostnames, err := net.LookupAddr(ipAddress)
+	if err != nil || len(hostnames) == 0 {
+		return "" // Return an empty string if no hostname is found or an error occurs
+	}
 
-        if scanner.Scan() {
-        ipAddress = strings.TrimSpace(scanner.Text())
-        } else {
-        fmt.Println("Error reading IP address from standard input.")
-        return
-        }
-        }
+	// Clean up the hostname by removing the trailing dot, if present
+	hostname := strings.TrimSuffix(hostnames[0], ".")
 
-        hostnames, _ := getHostnames(ipAddress)
-        if hostnames != nil {
-          fmt.Println(hostnames[0][:len(hostnames[0])-1])
-        return
-        }
+	// Return the cleaned-up hostname
+	return hostname
 }
